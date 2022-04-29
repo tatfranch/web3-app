b720a3cd94ba3d7f87d4be5885e9c5a222196f4b
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-04-29 14:09:26 +0200

    adding logo and installing icon packages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({ title, classprops }) => (
+  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+);
+
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <img src={logo} alt='logo' className='w-32 cursor-pointer' />
+      </div>
+      <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+        {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
