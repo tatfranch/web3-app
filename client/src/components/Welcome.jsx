3edbf040368914848c032472ca0196afb377d6cc
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-05-09 15:27:18 +0200

    hero image text and some styling

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,7 +5,17 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { Loader } from './';
 
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <div className='flex w-full justify-center items-center'>
+      <div className='flex md:flex-row flex-col items-start justify-between md:pd-20 py-12 px-4'>
+        <div className='flex flex-1 justify-start flex-col md:mr-10'>
+          <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
+            Send Crypto <br /> across the world
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
