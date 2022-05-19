b905ee5d33539aa174e750fd096673f7bc917870
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-05-19 18:24:21 +0200

    hard hat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-goerli.alchemyapi.io/v2/BILiV9oLWrCktcWfkUpGY9hImeex-LPC
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle');
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.alchemyapi.io/v2/BILiV9oLWrCktcWfkUpGY9hImeex-LPC',
+      accounts: [''],
+    },
+  },
 };
